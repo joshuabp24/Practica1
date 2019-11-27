66cb23196e0c713bc7c557726da6c2eaffc8b194
joshuabp24 <54971712+joshuabp24@users.noreply.github.com>
2019-11-26 17:51:11 -0800

    Add files via upload

---
+++ P.Evaluativa/P.Evaluativa/Operacion.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P.Evaluativa
+{
+    public class Operacion
+    {
+
+        public Persona BuscarPersona(int id)
+        {
+            var personas = ObtenerPersonas();
+            var p = (from persona in personas
+                     where persona.Id == id
+                     select persona).First();
+            return p;
+
+        }
+        public List<Persona> ObtenerPersonas()
+        {
+            var datos = ObtenerLineas();
+            List<Persona> personas = new List<Persona>();
+            foreach (var item in datos)
+            {
+                string[] info = item.Split(',');
+                Persona persona = new Persona
+                {
+                    Id = int.Parse(info[0]),
+                    Nombre = info[1],
+                    Profesion = info[2],
+                    Edad = int.Parse(info[3])
+                };
+                personas.Add(persona);
+            }
+            return personas;
+        }
+        public List<string> ObtenerLineas()
+        {
+            try
+            {
+                List<string> lineas = new List<string>();
+                string[] info = null;
+                if (File.Exists("Datos.txt"))
+                {
+                    info = File.ReadAllLines("Datos.txt");
+                }
+
+                foreach (string item in info)
+                {
+                    lineas.Add(item);
+                }
+                return lineas;
+            }
+            catch (System.Exception)
+            {
+
+
+            }
+            return null;
+        }
+
+    }
+} 
+
---
+++ P.Evaluativa/P.Evaluativa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P.Evaluativa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola ??");
+            Operacion op = new Operacion();
+            Console.WriteLine("Ingrese el id de una de las personas");
+            int temp = Convert.ToInt32(Console.ReadLine());
+            op.BuscarPersona(temp);
+            Console.ReadKey();
+        }
+    }
+}
